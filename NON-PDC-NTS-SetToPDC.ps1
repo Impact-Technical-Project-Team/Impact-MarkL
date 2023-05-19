811bb520feab0d90329e224f18541db618f55e5d
mlafnitzegger <83373139+mlafnitzegger@users.noreply.github.com>
2023-05-19 10:31:41 -0500

    NTS Solutions
    This PS is to assist with fixing NTS on a domain. The "SetPDC-NTS will set the PDC to update to the gov pooled time servers. The NON-PDC-NTS is to reset the member DCs and set NTS to reference the PDC.

---
+++ NON-PDC-NTS-SetToPDC.ps1
@@ -0,0 +1,11 @@
+﻿#Each of your non-PDCe domain controllers: First, reset the time service and set the DC back to its default:
+.\net.exe stop .\w32time.dll
+.\w32tm.exe /unregister
+.\w32tm.exe /register
+.\net.exe start .\w32time.dll
+# Then set the DC to sync its time with the PDCe (using the default time hierarchy)
+.\w32tm.exe /config /syncfromflags:domhier /update
+.\net.exe stop .\w32time.dll && .\net.exe start .\w32time.dll
+# If you have issues with the w32time service starting due to SID, run this: sc config w32time type=own  
+# You can then check to see how all of your DCs and PDCe are configured:
+.\w32tm.exe /monitor /domain:FQDN
---
+++ SetPDC-NTS.ps1
@@ -0,0 +1,8 @@
+﻿# First Set the PDC as the Time Keeper
+# Execute this PS on the PDC
+.\net.exe stop .\w32time.dll
+.\w32tm.exe /config /syncfromflags:manualpeerlist:"0.us.pool.ntp.org,1.us.pool.ntp.org,2.us.pool.ntp.org,3.us.pool.ntp.org"
+.\net.exe start .\w32time.dll
+.\w32tm.exe /query /configuration
+.\w32tm.exe /query /status
+.\w32tm.exe /resync /rediscover
